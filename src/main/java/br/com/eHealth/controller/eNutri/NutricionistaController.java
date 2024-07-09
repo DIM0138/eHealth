a7cc00170057ffecf394ee5805f70f842051f197
Franklin Oliveira <franklin.oliveira.049@ufrn.edu.br>
2024-07-09 20:30:07 -0300

    fix: atualização de relatórios

--- src/main/java/br/com/eHealth/controller/eNutri/NutricionistaController.java
+++ src/main/java/br/com/eHealth/controller/eNutri/NutricionistaController.java
@@ -6,17 +6,12 @@ package br.com.eHealth.controller.eNutri;
 import br.com.eHealth.controller.eHealth.UsuarioController;
 import br.com.eHealth.model.eNutri.Nutricionista;
 import br.com.eHealth.model.eNutri.dto.NutricionistaDTO;
-import br.com.eHealth.service.eNutri.NutricionistaService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/nutricionistas")
+@RequestMapping("/enutri/nutricionistas")
 public class NutricionistaController extends UsuarioController<Nutricionista, NutricionistaDTO> {
-
-    @Autowired
-    private NutricionistaService nutricionistaService;
 
 }
--- src/main/java/br/com/eHealth/repository/eHealth/RelatorioRepository.java
+++ src/main/java/br/com/eHealth/repository/eHealth/RelatorioRepository.java
@@ -1,6 +1,7 @@
 package br.com.eHealth.repository.eHealth;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import br.com.eHealth.model.eHealth.Relatorio;
 @Repository
 public interface RelatorioRepository<T extends Relatorio> extends JpaRepository<T, Long>{
     Boolean existsByDataConsultaAndPacienteId(LocalDate data, Long id);
+    Optional<T> findByDataConsultaAndPacienteId(LocalDate data, Long id);
+    List<T> findByPacienteId(Long id);
 }
